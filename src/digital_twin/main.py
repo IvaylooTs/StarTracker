1e093075bf15affe91e449df18af351d02c75234
Didi0dum <Deedee.vasileva@gmail.com>
2025-08-04 13:26:36 +0300

    ☆ Angular distance calculation optimization
    - using matrix multiplication instead of for loops. Time complexity is still O(N^2), but constant factors are reduced. Numpy uses BLAS C interfaces => faster

--- src/digital_twin/main.py
+++ src/digital_twin/main.py
@@ -126,21 +126,22 @@ def star_coords_to_unit_vector(star_coords, center_coords, f_x, f_y):
         
     return np.array(unit_vectors)
 
+# Angular distance between each unique pair of unit vectors
+# Inputs:
+# - unit_vectors: np array containing [x, y, z] coordinates of each unit vector
+# Outputs:
+# - angular_dists: dict where {(star_index1 [-], star_index2 [-]): angular_distance [deg]} 
 def angular_dist_helper(unit_vectors):
-    num_of_vectors = len(unit_vectors)
-    angular_distances = {}
-    for i in range(0, num_of_vectors):
-        for j in range(i + 1, num_of_vectors):
-            vector1 = unit_vectors[i]
-            vector2 = unit_vectors[j]
-            
-            dot_product = np.dot(vector1, vector2)
-            dot__product_clamped = np.clip(dot_product, -1.0, 1.0)
-            
-            angular_dist_rad = np.arccos(dot__product_clamped)
-            angular_dist_deg = np.degrees(angular_dist_rad)
-            angular_distances[(i, j)] = angular_dist_deg
-    return angular_distances
+    # Multiply unit vector matrix by it's transpose to find the dot products
+    dot_products = unit_vectors @ unit_vectors.T
+    # clip values to an interval of [-1.0, 1.0] (arccos allowed values) to account for float rounding error
+    dot_products = np.clip(dot_products, -1.0, 1.0)
+    
+    angular_dist_matrix = np.degrees(np.arccos(dot_products))
+    # Create a dict with all unique combination from the angular distance matrix
+    angular_dists = {(i, j): angular_dist_matrix[i, j]
+                     for i, j in combinations(range(len(unit_vectors)), 2)}
+    return angular_dists
 
 def get_angular_distances(star_coords, center_coords, f_x, f_y):
     unit_vectors = star_coords_to_unit_vector(star_coords, center_coords, f_x, f_y)
